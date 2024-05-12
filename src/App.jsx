68f2cfdcb8caa9c1164bb70c6132deb3e3aa1465
Huy01040104 <tronghuy01040@gmail.com>
2024-05-12 17:16:36 +0700

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App() {
     const [count, setCount] = useState(0);
+    const value = 123456;
 
     return (
         <>
@@ -23,7 +24,7 @@ function App() {
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count} 123456
+                    count is {count} {value}
                 </button>
                 <p>
                     Edit <code>src/App.jsx</code> and save to test HMR
